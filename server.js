8d0bb67a1c5b4f29971a6737654c5dd3eb58121b
sudheergpalli <30798008+sudheergpalli@users.noreply.github.com>
2017-08-08 23:14:41 +0530

    with message1 , 2 , 3 java script objects

--- server.js
+++ server.js
@@ -9,24 +9,28 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+// Java Script Object for message1
 var message1 = {
     title : "Messsage 1 Page",
     date  : "Date is 8th Aug 2017",
     header : "I am in Mesage 1 Page"
 };
 
+// Java Script Object for message2
 var message2 = {
     title : "Messsage 2 Page",
     date  : "Date is 8th Aug 2018",
     header : "I am in Mesage 2 Page"
 };
 
+// Java Script Object for message3
 var message3 = {
     title : "Messsage 3 Page",
     date  : "Date is 8th Aug 2019",
     header : "I am in Mesage 3 Page"
 };
 
+// function to return html using java script object as an argument 
 function createhtml(data){
     var title = data.title;
     var date = data.date;
@@ -53,19 +57,19 @@ function createhtml(data){
 app.get('/message1', function (req, res) {
   // res.send("First Message is Displayed");
   //res.sendFile(path.join(__dirname, 'ui', 'message1.html'));
-  res.send(createhtml(message1));
+  res.send(createhtml(message1));  // sending 'message1' java script object as an parameter to function 'createhtml' that retruns 'html' page
 });
 
 app.get('/message2', function (req, res) {
   //res.send("Second Message is Displayed");
   //res.sendFile(path.join(__dirname, 'ui', 'message2.html'));
-  res.send(createhtml(message2));
+  res.send(createhtml(message2)); // sending 'message2' java script object as an parameter to function 'createhtml' that retruns 'html' page
 });
 
 app.get('/message3', function (req, res) {
   //res.send("Third Message is Displayed");
   //res.sendFile(path.join(__dirname, 'ui', 'message3.html'));
-  res.sendFile(createhtml(message3));
+  res.sendFile(createhtml(message3)); // sending 'message3' java script object as an parameter to function 'createhtml' that retruns 'html' page
 });
 
 
